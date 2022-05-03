2ec7fb775434a9640f0f847c4fa1ca3f655297f6
sgoyal100 <sgoyal.libra@gmail.com>
2022-05-02 23:28:57 -0700

    Adding Decode string solution

--- DecodeString.cs
+++ DecodeString.cs
@@ -1,7 +1,52 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(n)
+// Space Complexity : O(h)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
 
 
 // Your code here along with comments explaining your approach
+
+public string DecodeString(string s) {
+        
+        if(string.IsNullOrEmpty(s))
+            return null;
+        
+        Stack<StringBuilder> strStack = new Stack<StringBuilder>();
+        Stack<int> numStack = new Stack<int>();
+        
+        int currNum = 0;
+        StringBuilder currString = new StringBuilder();
+        
+        for(int i = 0; i < s.Length; i++)
+        {
+            char c = s[i];
+            if(Char.IsDigit(c))
+            {                
+                currNum = currNum * 10 + (c - '0');
+            }
+            else if(c == '[')
+            {
+                numStack.Push(currNum);
+                strStack.Push(currString);
+                currNum = 0;
+                currString = new StringBuilder();
+            }
+            else if(c == ']')
+            {
+                int times = numStack.Pop();
+                var newStr = new StringBuilder();
+                for(int j = 0; j < times; j++)
+                {
+                    newStr.Append(currString);
+                }
+                currString = strStack.Pop().Append(newStr);
+            }
+            else
+            {
+                
+                currString.Append(c);
+            }
+        }
+        return currString.ToString();
+    }    
+}
